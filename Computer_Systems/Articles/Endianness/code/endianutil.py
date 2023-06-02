cf19aee10424a15056989ed4d55b16b2ea353f49
ThaTechMaestro <muse.pearls@gmail.com>
2023-06-02 03:00:08 +0100

    Endian Article: extra comments

--- Computer_Systems/Articles/Endianness/code/endianutil.py
+++ Computer_Systems/Articles/Endianness/code/endianutil.py
@@ -1,11 +1,14 @@
-# https://blockchain-academy.hs-mittweida.de/litte-big-endian-converter/
 import re
 
-
 class EndianUtil:
-
-    '''
-    '''
+    """
+    EndianUtil is a class to perform reversed byte order operations\n
+    This reversals are carried out both ways\n
+    Inputs following Big Endian Order are returned\n
+        in Little Endian Order & vice versa
+    
+    Simulating: https://blockchain-academy.hs-mittweida.de/litte-big-endian-converter/
+    """ 
 
     def is_valid_hex(self, val):
 
@@ -13,11 +16,26 @@ class EndianUtil:
         return bool(re.match(hex_pattern, val))
 
     def is_valid_binary(self, val):
-
+      
         binary_pattern = "^[01]+$"
         return bool(re.match(binary_pattern, val))
 
     def pad_hex_val(self, val):
+        """
+        Adds leading zeros to hex argument based on some conditions\n
+        Also referred to as padding
+        
+        Example:
+            val = a;
+            res = pad_hex_val(val);
+            res -> 0a;
+
+        Args:
+            val (str): hexadecimal value
+
+        Returns:
+            str: padded value or passed in hex argument without padding
+        """
 
         pad_val = str(0)
         pad_length = len(val) % 2
@@ -29,6 +47,21 @@ class EndianUtil:
         return val
 
     def pad_binary_val(self, val):
+        """
+        Adds leading zeros to binary argument based on some conditions
+        Also referred to as padding
+        
+        Example:
+            val = 11;
+            res = pad_binary_val(val);
+            res -> 0011;
+
+        Args:
+            val (str): binary value
+
+        Returns:
+            str: padded value or passed in binary argument without padding
+        """
 
         pad_val = str(0)
         pad_length = len(val) % 4
@@ -40,6 +73,21 @@ class EndianUtil:
         return val
 
     def reverse_hex_endianness(self, val):
+        """
+        Reverses the endian order of passed in hex argument\n
+        Big endian to little endian & vice versa
+        
+        Example:
+            val = abcd;
+            res = reverse_hex_endianness(val);
+            res -> cdab;
+
+        Args:
+            val (str): hex value
+
+        Returns:
+            str: reversed byte order of passed in hex argument
+        """
 
         padded_hex_val = self.pad_hex_val(val)
         bytes_data = bytes.fromhex(padded_hex_val)
@@ -48,6 +96,21 @@ class EndianUtil:
         return reversed_bytes.hex()
 
     def reverse_bin_endianness(self, val):
+        """
+        Reverses the endian order of passed in binary argument\n
+        Big endian to little endian & vice versa
+        
+        Example:
+            val = 1111111110101010;
+            res = reverse_bin_endianness(val);
+            res -> 1010101011111111;
+
+        Args:
+            val (str): binary value
+
+        Returns:
+            str: reversed byte order of passed in binary argument
+        """
 
         padded_bin_val = self.pad_binary_val(val)
         hex_val = hex(int(padded_bin_val, 2))[2:]
@@ -58,6 +121,16 @@ class EndianUtil:
         return bin(int(reversed_endian_hex, 16))[2:]
 
     def display_reversed_endian_val(self, val):
+        """
+        Prints reversed endian order of binary/hex argument
+
+        Args:
+            val (str): binary or hex value
+
+        Raises:
+            ValueError: Empty strings
+            ValueError: Invalid hex or binary argument
+        """
 
         if not val:
             raise ValueError("Empty string not allowed")
@@ -80,4 +153,3 @@ if __name__ == '__main__':
     val = str(input('Enter Hex/Binary value: ')).strip().lower()
     endianUtil = EndianUtil()
     endianUtil.display_reversed_endian_val(val)
-    # print(a.display_reversed_endian_val("0101"))
